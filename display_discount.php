6a349b2bb38b97eb5a66022c0b62d1cae3f1abc5
Huynhthithuyhau25ITE016 <hauhtt.25ite@vku.udn.vn>
2026-09-14 21:38:02 +0700

    Bước 8 - Định dạng dữ liệu
    Formatted output values for list price, discount percent, discount amount, and discount price.

--- display_discount.php
+++ display_discount.php
@@ -5,6 +5,11 @@ $discount_percent = filter_input(INPUT_POST, 'discount_percent', FILTER_VALIDATE
 
 $discount = $list_price * $discount_percent * 0.01;
 $discount_price = $list_price - $discount;
+
+$list_price_f = number_format($list_price, 2);
+$discount_percent_f = number_format($discount_percent, 1) . '%';
+$discount_f = number_format($discount, 2);
+$discount_price_f = number_format($discount_price, 2);
 ?>
 <!DOCTYPE html>
 <html>
@@ -20,15 +25,15 @@ $discount_price = $list_price - $discount;
         <span><?php echo $product_description; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo $list_price; ?></span><br>
+        <span><?php echo $list_price_f; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo $discount_percent; ?></span><br>
+        <span><?php echo $discount_percent_f; ?></span><br>
         <label>Discount Amount:</label>
-        <span><?php echo $discount; ?></span><br>
+        <span><?php echo $discount_f; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo $discount_price; ?></span><br>
+        <span><?php echo $discount_price_f; ?></span><br>
     </main>
 </body>
 </html>
